407d2700acf9aef83a93caa9eae6db40a8985624
DVMaja <majadreilinger@gmail.com>
2023-02-22 15:48:04 +0100

    összeadások szétszedve

--- src/metodusokgyak/MetodusokGyak.java
+++ src/metodusokgyak/MetodusokGyak.java
@@ -39,21 +39,13 @@ public class MetodusokGyak {
         kiir(szoveg);
     }
 
-    private static void Osszeadasok() {
-         //Összeadás
+    private static void Osszeadasok() {        
         szam1 = 1;
         szam2 = 2;
         szam3 = 4;
-        szam4 = 3;        
-        osszeg = osszead(szam1, szam2);
-        szoveg = ("%d + %d = %d".formatted(szam1, szam2, osszeg));
-        kiir(szoveg);
-        
-        //Összeadás
-        osszeg = osszead(1, 2);
-        osszeg = osszead(osszeg, szam3);
-        osszeg = osszead(osszeg, szam4); 
-        kiir("%d + %d + %d + %d = %d".formatted(szam1, szam2, szam3, szam3, osszeg));        
+        szam4 = 3;    
+        Osszeadas1();
+        Ossszeadas2();                     
     }
 
     private static void Gyokvonas() {
@@ -65,6 +57,19 @@ public class MetodusokGyak {
        double gyok = Math.sqrt(osszeg);
        String kimenet = String.format("%s %.4f\n",s, gyok );
        kiir(kimenet);        
+    }
+
+    private static void Osszeadas1() {
+        osszeg = osszead(szam1, szam2);
+        szoveg = ("%d + %d = %d".formatted(szam1, szam2, osszeg));
+        kiir(szoveg);
+    }
+
+    private static void Ossszeadas2() {
+        osszeg = osszead(1, 2);
+        osszeg = osszead(osszeg, szam3);
+        osszeg = osszead(osszeg, szam4); 
+        kiir("%d + %d + %d + %d = %d".formatted(szam1, szam2, szam3, szam3, osszeg));  
     }
    
 }
