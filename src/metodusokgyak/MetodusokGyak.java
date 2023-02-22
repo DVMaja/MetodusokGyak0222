59922459c0b4904321e76e86ab7b9fd0386c6622
DVMaja <majadreilinger@gmail.com>
2023-02-22 14:48:23 +0100

    kiiratás nem működik

--- src/metodusokgyak/MetodusokGyak.java
+++ src/metodusokgyak/MetodusokGyak.java
@@ -3,7 +3,12 @@ package metodusokgyak;
 public class MetodusokGyak {
 
     public static void main(String[] args) {
-        
+        elso10SzamOsszege();
+        String szoveg = "Az első 10 szám összege %d\\n\",osszeg";
+        kiir(szoveg);
+        osszead(2, 3);
+        szoveg = "%d + %d = %d";
+        kiir(szoveg);
     }
     
     private static int elso10SzamOsszege(){
@@ -11,20 +16,18 @@ public class MetodusokGyak {
         for (int i = 0; i < 10; i++) {
             osszeg += i;
         }
-        return osszeg;
-        
+        return osszeg;        
     }
 
-    private static void eredmenyKiiras(int osszeg) {
-        System.out.printf("Az első 10 szám összege %d\n",osszeg);
-    }
+//    private static void eredmenyKiiras(int osszeg) {
+//        System.out.printf("Az első 10 szám összege %d\n",osszeg);
+//    }
 
-    private static int osszead(int a, int b) {
-        int osszeg = a + b;
-        return osszeg;
+    private static int osszead(int a, int b) {        
+        return a + b;
     }
     
-    private static void osszeadasKiiras(int a, int b, int osszeg) {
-        System.out.printf("%d + %d = %d\n",osszeg);
+    private static void kiir(String szoveg) {
+        System.out.printf(szoveg);
     }
 }
