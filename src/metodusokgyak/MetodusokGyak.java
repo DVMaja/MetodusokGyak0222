90c961bc896ce472b13159fbcb7e9424d253a361
DVMaja <majadreilinger@gmail.com>
2023-02-22 14:37:06 +0100

    javított kiírás, üres mainű

--- src/metodusokgyak/MetodusokGyak.java
+++ src/metodusokgyak/MetodusokGyak.java
@@ -3,13 +3,7 @@ package metodusokgyak;
 public class MetodusokGyak {
 
     public static void main(String[] args) {
-        int osszeg = elso10SzamOsszege();
-        eredmenyKiiras(osszeg);
         
-        int a = 1;
-        int b = 2;
-        osszeg = osszead(a, b);
-        eredmenyKiiras(osszeg);
     }
     
     private static int elso10SzamOsszege(){
@@ -30,5 +24,7 @@ public class MetodusokGyak {
         return osszeg;
     }
     
-    
+    private static void osszeadasKiiras(int a, int b, int osszeg) {
+        System.out.printf("%d + %d = %d\n",osszeg);
+    }
 }
