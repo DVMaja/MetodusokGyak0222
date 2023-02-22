d8d1b8d38299d87199b457419f5ba8b31104f3f2
DVMaja <majadreilinger@gmail.com>
2023-02-22 15:45:29 +0100

    feladat egységekre bontott, de összeadas még egybe

--- src/metodusokgyak/MetodusokGyak.java
+++ src/metodusokgyak/MetodusokGyak.java
@@ -1,35 +1,18 @@
 package metodusokgyak;
 
 public class MetodusokGyak {
+    
+    static int szam1, szam2, szam3, szam4, osszeg;
+    static String szoveg;
 
     public static void main(String[] args) {
         feladatok();
     }
     
      private static void feladatok() {
-         int osszeg = elso10SzamOsszege();
-        String szoveg = "Az első 10 szám összege" + osszeg;
-        kiir(szoveg);
-        
-        int szam1 = 1, szam2 = 2, szam3 = 4, szam4 = 3;        
-        osszeg = osszead(szam1, szam2);
-        szoveg = ("%d + %d = %d".formatted(szam1, szam2, osszeg));
-        kiir(szoveg);
-        
-        osszeg = osszead(1, 2);
-        osszeg = osszead(osszeg, szam3);
-        osszeg = osszead(osszeg, szam4); 
-        kiir("%d + %d + %d + %d = %d".formatted(szam1, szam2, szam3, szam3, osszeg));
-               
-       
-       int a = 3, b = 4, c = 2;
-       osszeg = osszead(a,b);
-       osszeg = osszead(osszeg, c);
-       String s ="%d + %d + %d gyöke".formatted(a, b, c);
-       double gyok = Math.sqrt(osszeg);
-       String kimenet = String.format("%s %.4f\n",s, gyok );
-        kiir(kimenet);
-        
+         SzamOsszegeElsoF();
+         Osszeadasok();
+         Gyokvonas();        
     }
     
     private static int elso10SzamOsszege(){
@@ -49,5 +32,39 @@ public class MetodusokGyak {
         System.out.printf(szoveg +"\n");
     }   
 
+    private static void SzamOsszegeElsoF() {
+        //Első 10 számjegy összege
+        osszeg = elso10SzamOsszege();
+        szoveg = "Az első 10 szám összege" + osszeg;
+        kiir(szoveg);
+    }
+
+    private static void Osszeadasok() {
+         //Összeadás
+        szam1 = 1;
+        szam2 = 2;
+        szam3 = 4;
+        szam4 = 3;        
+        osszeg = osszead(szam1, szam2);
+        szoveg = ("%d + %d = %d".formatted(szam1, szam2, osszeg));
+        kiir(szoveg);
+        
+        //Összeadás
+        osszeg = osszead(1, 2);
+        osszeg = osszead(osszeg, szam3);
+        osszeg = osszead(osszeg, szam4); 
+        kiir("%d + %d + %d + %d = %d".formatted(szam1, szam2, szam3, szam3, osszeg));        
+    }
+
+    private static void Gyokvonas() {
+        //Gyökvonás
+       int a = 3, b = 4, c = 2;
+       osszeg = osszead(a,b);
+       osszeg = osszead(osszeg, c);
+       String s ="%d + %d + %d gyöke".formatted(a, b, c);
+       double gyok = Math.sqrt(osszeg);
+       String kimenet = String.format("%s %.4f\n",s, gyok );
+       kiir(kimenet);        
+    }
    
 }
