f1aacf2b279c835ba0cb4fbea635688f16273a8f
DVMaja <majadreilinger@gmail.com>
2023-02-24 11:57:21 +0100

    paraméteres és véletlenszámos kiírás

--- src/metodusokgyak/Equalizer.java
+++ src/metodusokgyak/Equalizer.java
@@ -8,27 +8,34 @@ public class Equalizer {
     
     public static void main(String[] args) {
         SzinesMintak();
+        
     }
 
     private static void SzinesMintak() {
-        eq();
-        eq();
-        eq();
-        eq();
-        eq();
+        int szam = RandomSzam();
+        eq(szam);
+        eq(12);//12
+        szam = RandomSzam();
+        eq(szam);
+        eq(8);//8
+        szam = RandomSzam();
+        eq(szam);
     }
 
-    private static void eq() {
+    private static void eq(int a) {
         //int felso = 7, also = 3;
-        String szin = "\u001B[45m";
-        
-        int mennyiseg = rnd.nextInt(3, 8);
-        for (int i = 0; i < mennyiseg; i++) {
+        String szin = "\u001B[45m";        
+       
+        for (int i = 0; i < a; i++) {
             System.out.print(szin + "*");
             
         }
-        System.out.println("");
+        System.out.println("");    
         
-        
+    }
+
+    private static int RandomSzam() {
+        int mennyiseg = rnd.nextInt(3, 8);
+        return mennyiseg;
     }
 }
