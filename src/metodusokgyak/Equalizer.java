9933593779baa9f225777ea0c8ea5091b3860f97
DVMaja <majadreilinger@gmail.com>
2023-02-24 12:09:34 +0100

    túlterheléses változat

--- src/metodusokgyak/Equalizer.java
+++ src/metodusokgyak/Equalizer.java
@@ -7,7 +7,8 @@ public class Equalizer {
     private static Random rnd = new Random();
     
     public static void main(String[] args) {
-        SzinesMintak();
+        //SzinesMintak();
+        TulterhelésesValtozat();
         
     }
 
@@ -21,21 +22,36 @@ public class Equalizer {
         szam = RandomSzam();
         eq(szam);
     }
+    
+    private static void TulterhelésesValtozat() {
+        eq();
+        eq(12);
+        eq();
+        eq(8);
+        eq();
+        
+    }
 
     private static void eq(int a) {
         //int felso = 7, also = 3;
         String szin = "\u001B[45m";        
        
         for (int i = 0; i < a; i++) {
-            System.out.print(szin + "*");
-            
+            System.out.print(szin + "*");            
         }
-        System.out.println("");    
-        
-    }
-
+        System.out.println("");           
+    }  
+    
+    /*Túlterhelt metódus: eltér a paraméter szám és / vagy típus*/
+    private static void eq() {
+        int db = rnd.nextInt(3, 8);
+        eq(db);        
+    }   
+    
     private static int RandomSzam() {
         int mennyiseg = rnd.nextInt(3, 8);
         return mennyiseg;
     }
+
+    
 }
