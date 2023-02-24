2d5221a8eb881271aeb8de334efdb20b05dac2e2
DVMaja <majadreilinger@gmail.com>
2023-02-24 12:38:25 +0100

    lefut de van egy figyelmeztető üzenet

--- src/metodusokgyak/Equalizer.java
+++ src/metodusokgyak/Equalizer.java
@@ -3,13 +3,13 @@ package metodusokgyak;
 import java.util.Random;
 
 public class Equalizer {
-    
+
     private static Random rnd = new Random();
-    
+
     public static void main(String[] args) {
         //SzinesMintak();
         TulterhelésesValtozat();
-        
+
     }
 
     private static void SzinesMintak() {
@@ -22,48 +22,47 @@ public class Equalizer {
         szam = RandomSzam();
         eq(szam);
     }
-    
+
     private static void TulterhelésesValtozat() {
         eq();
         eq(8, true);
         eq(12);
         eq(true);
         eq(false);
-        
+
     }
 
-    private static void eq(int a) {
+    private static void eq(int db, boolean igaz) {
         //int felso = 7, also = 3;
-        String szin = "\u001B[45m";        
-       
-        for (int i = 0; i < a; i++) {
-            System.out.print(szin + "*");            
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            System.out.print(szin + "*");
         }
-        System.out.println("");           
-    }  
-    
+
+        if (igaz == true) {
+            System.out.print(" (" + db + ") ");
+        }
+        System.out.println("");
+    }
+
+    private static void eq(int db) {
+        eq(db, false);
+    }
+
     /*Túlterhelt metódus: eltér a paraméter szám és / vagy típus*/
     private static void eq() {
         int db = rnd.nextInt(3, 8);
-        eq(db);        
-    }  
-    
-    private static void eq(boolean igaz) {
-        if (igaz == true) {
-            eq(4);
-        }else{
-            eq();        
-        }     
-    }   
-    //rivate static void eq(int szam, boolean igaz) {
-        
-    //}
-    
-    
-    private static int RandomSzam() {
-        int mennyiseg = rnd.nextInt(3, 8);
-        return mennyiseg;
+        eq(db);
     }
 
-    
+    private static void eq(boolean igaz) {
+        int db = rnd.nextInt(3, 8);
+        eq(db, igaz = true);
+    }
+
+//    private static int RandomSzam() {
+//        int mennyiseg = rnd.nextInt(3, 8);
+//        return mennyiseg;
+//    }
+//
 }
